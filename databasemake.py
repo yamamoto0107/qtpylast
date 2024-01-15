c90639c57a94ad5096b15fee7fa75509778948ac
yamamoto0107 <yamamoto@jecai.onmicrosoft.com>
2024-01-15 11:41:01 +0900

    データベース作成＿attend

---
+++ databasemake.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+def createattend():
+    dbname = 'attendsystem.db'
+    conn = sqlite3.connect(dbname)
+    #SQLを操作するカーソルオブジェクトを作成
+    c = conn.cursor()
+    sql="""
+        CREATE TABLE attend(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        number INTEGER,
+        name VARCHAR(20),
+        kana VARCHAR(50),
+        gender VARCHAR(10),
+        mail VARCHAR(100),
+        abbre VARCHAR(50)        
+        );
+        """
+    conn.execute(sql)
+    conn.close()
+createattend()
