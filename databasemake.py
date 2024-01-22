4e28ff5772f752af997daeccda22aeba8f9a27e1
yamamoto0107 <yamamoto@jecai.onmicrosoft.com>
2024-01-22 12:19:38 +0900

    データベースにinsert,selectを追加

--- databasemake.py
+++ databasemake.py
@@ -18,4 +18,36 @@ def createattend():
 
     conn.execute(sql)
     conn.close()
-createattend()
+
+def insertattend():
+    num = 19002
+    name = "山本涼平"
+    dbname = 'attendsystem.db'
+    conn = sqlite3.connect(dbname)
+    #SQLを操作するカーソルオブジェクトを作成
+    c = conn.cursor()
+    sql="INSERT INTO attend (number,name) values (?,?)"
+    data = (num,name)
+    conn.execute(sql,data)
+    conn.commit()
+    conn.close()
+
+def selectattend():
+    dbname = 'attendsystem.db'
+    conn = sqlite3.connect(dbname)
+    #SQLを操作するカーソルオブジェクトを作成
+    c = conn.cursor()
+    sql = "SELECT * FROM attend"
+    c.execute(sql)
+    rows = c.fetchall()
+    print(len(rows),"行のデータがありました")
+    temp =[]
+    for row in rows:
+        #print(type(row),row)
+        temp.append(row)
+    conn.close()
+    return temp
+#createattend()
+#insertattend()
+temp = selectattend()
+print(temp)
