d237976dadbe4fc3bfc69b2702ca933e92b77cff
kunimoto12 <ai22006@jecai.onmicrosoft.com>
2024-01-22 10:52:45 +0900

    名前変更3

---
+++ attend0115.py
@@ -0,0 +1,60 @@
+﻿import sys
+from tkinter import Tk
+from PyQt6.QtWidgets import QPushButton, QLabel,QApplication, QPushButton, QWidget, QLineEdit
+import face_recognition_test as ft
+import numpy as np
+import face_signup as up
+import face_signin as signin
+
+root = Tk()
+monitor_height = root.winfo_screenheight()
+monitor_width = root.winfo_screenwidth()
+
+
+class Main(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('JECAIportal') # ウィンドウのタイトル
+        self.setGeometry(0,0,monitor_width,monitor_height) # ウィンドウの位置と大きさ
+        self.initUi()
+    
+    def initUi(self):
+        self.button = QPushButton('出席登録', self)
+        self.button.clicked.connect(self.team1)
+
+    def team1(self):
+        self.w1 = FaceWindow()
+        self.w1.show()
+
+# 顔認識
+class FaceWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('JECAIportal') # ウィンドウのタイトル
+        self.setGeometry(0,0,monitor_width,monitor_height) # ウィンドウの位置と大きさ
+        self.initUi()
+    def initUi(self):
+        self.label = QLineEdit(self)
+        self.label.move(120, 50)
+        self.button = QPushButton("登録", self)
+        self.button.move(100, 10)
+        self.button1 = QPushButton("出席", self)
+        self.button1.move(200, 10)
+        self.button.clicked.connect(self.toroku)
+        self.button1.clicked.connect(self.syusseki)
+
+    def toroku(self):
+        self.w = up.Main()
+        self.w.show()
+
+    def syusseki(self):
+        self.w = signin.main()
+        self.label.setText(self.w)
+
+    
+#ここから下は変更NG
+if __name__ == '__main__':
+    App = QApplication(sys.argv)
+    window = Main()
+    window.show()
+    App.exec()
