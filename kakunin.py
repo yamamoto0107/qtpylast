9e44dcb81089437076034239f0ae441af908325f
ryunosuke0511 <ai22014@jecai.onmicrosoft.com>
2024-01-24 12:10:54 +0900

    完成（仮）

--- kakunin.py
+++ kakunin.py
@@ -5,6 +5,7 @@ import sqlite3
 import datetime
 #import jpholiday
 from PyQt6.QtWidgets import QPushButton
+import datetime
 root = Tk()
 monitor_height = root.winfo_screenheight()
 monitor_width = root.winfo_screenwidth()
@@ -30,7 +31,7 @@ class Main(QWidget):
         self.year.setMaxLength(4)
         self.label3 = QLabel("月", self)
         self.month = QComboBox()
-        for s in ['4月','5月','6月','7月','8月','9月','10月','11月','12月','1月','2月','3月']:
+        for s in ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']:
             self.month.addItem(s)
         self.button = QPushButton('出席率確認', self)
 
@@ -57,34 +58,49 @@ class Main(QWidget):
             self.w.show()
 
 class AnotherWindow(QWidget):# 出席コマの検査59から74まで追加
-    def susseki():
-        conn = sqlite3.connect('record.db')
+    def syusseki(self):
+        conn = sqlite3.connect('record.db',detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+
+        # "TIMESTAMP"コンバータ関数 をそのまま ”DATETIME” にも使う
+        sqlite3.dbapi2.converters['DATETIME'] = sqlite3.dbapi2.converters['TIMESTAMP']
+
         cursor = conn.cursor()
-        table_name = 'record'
-        query = f"SELECT * ,
-                FROM record,
-                WHERE number = (self.number) AND date = (self.month)
-                "
-        
-        result = cursor.fetchall()
-
-        print(result)
-
-
-
+        query = "SELECT * FROM record WHERE number = ? AND date > ?"
+        data=(self.number,datetime.datetime(int(self.year),int(self.month),1))#,datetime.datetime(int(self.year),int(self.month),31))
+        print(data)
+        result = cursor.execute(query,data)
+        a=result.fetchall()
+        a=len(a)
+        conn.close()
+        return a
 
 
     def __init__(self,number,year,month):
         super().__init__()
         # グリッド(Grid)レイアウトの作成とメインウィンドウへの設定
+        self.number = number
+        self.year =year
+        listm = ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']
+        for i in range(len(listm)):
+            if month == listm[i]:
+                if i < 10:
+                    month = "0"+str(i+1)
+                else:
+                    month = str(i+1)
+                break
+
+        self.month = month
+        a = self.syusseki()
+        print(a)
         grid_layout = QGridLayout()
         self.setLayout(grid_layout)
 
-        self.label1 = QLabel("出席コマ数", self)
+        self.label1 = QLabel("出席コマ数",self)
         self.label2 = QLabel("曜日ごとの欠席コマ数", self)
         self.label3 = QLabel("合計欠席コマ数", self)
         self.label4 = QLabel("遅刻コマ数", self)
         self.label5 = QLabel("出席率", self)
+
 
         # レイアウトにウィジェットを追加
         grid_layout.addWidget(QLabel(number+' '+year+'年'+' '+month),0,0)
@@ -93,6 +109,7 @@ class AnotherWindow(QWidget):# 出席コマの検査59から74まで追加
         grid_layout.addWidget(self.label3,1,3)
         grid_layout.addWidget(self.label4,1,4)
         grid_layout.addWidget(self.label5,1,5)
+        grid_layout.addWidget(QLabel(str(a)),2,1)
 
     
 #ここから下は変更NG
